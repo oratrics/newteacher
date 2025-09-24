799f4586282819845233a77102c78de25e2494d4
adarshr7007 <adarshrastogi7007@gmail.com>
2025-09-24 14:04:17 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://backend.oratrics.in',
         changeOrigin: true,
         secure: false,
         configure: (proxy, _options) => {
